0056982f74b131c7e9b18fc83892b5d295ebfead
Giuliano Sposito <gsposito@ciandt.com>
2018-08-05 18:22:52 -0300

    Corrigindo inportação de proventos tratando linha com amortizacoes nao importando duplicatas na correcao (falta corrigir).

--- R/_draft/fiis_site_scrapper_proventos.R
+++ R/_draft/fiis_site_scrapper_proventos.R
@@ -1,7 +1,7 @@
 library(rvest)
 library(tidyverse)
 library(lubridate)
-source("./fii/common.R")
+source("./R/_draft/common.R")
 source("./fii/portfolio.R")
 
 .PROVENTOS_FILENAME <- "./fii/data/fii_proventos.rds"
@@ -10,23 +10,48 @@ source("./fii/portfolio.R")
   pg %>%
     html_nodes("div.entry-content ul li") %>%
     html_text() %>%
-    .[grep("distribuição.*Rendimento",.)] %>%
+    str_to_lower() %>% 
+    str_subset(".*informou.*distribui.*rendimento.*") %>% 
     str_extract_all("(\\d\\d\\/\\d\\d\\/\\d+)|(R\\$ \\d+,\\d*)|(\\d+,\\d*)",T) %>%
-    as.tibble() %>%
+    as.tibble() -> extraction
+  
+  extraction %>%
+    select(1:6) %>%
     set_names(c("data.update", "valor","data.pagamento",
                 "data.base","cota.base","rendimento")) %>%
     mutate(
       data.update = dmy(data.update),
       valor       = .parseRealValue(valor),
       data.pagamento = dmy(data.pagamento), 
-      data.base = dmy(data.base), 
-      cota.base = .parseRealValue(cota.base),
-      rendimento = .parseNumPtBr(rendimento)
+      data.base  = dmy(data.base), 
+      cota.base  = .parseNumPtBr(cota.base),
+      rendimento = .parseNumPtBr(rendimento)/100
     ) %>%
-    distinct() %>%
-    return()
+    distinct() %>% 
+    filter(complete.cases(.)) -> rendimentos
+  
+  if(ncol(extraction)>6) {
+    extraction %>% 
+      select(1, 7, 8, 9, 5) %>% 
+      set_names(c("data.update", "valor","data.pagamento",
+                  "data.base","cota.base")) %>%
+      mutate(
+        data.update = dmy(data.update),
+        valor       = .parseRealValue(valor),
+        data.pagamento = dmy(data.pagamento), 
+        data.base  = dmy(data.base), 
+        cota.base  = .parseNumPtBr(cota.base),
+        rendimento = (valor/cota.base)
+      ) %>%
+      distinct() %>% 
+      filter(complete.cases(.)) %>%
+      bind_rows(rendimentos) -> rendimentos
+  }
+  
+  return(rendimentos)
 }
 
+.safe_processProventos <- safely(.processProventos,NA)
 
 updateProventos <- function(){
 
@@ -35,16 +60,16 @@ updateProventos <- function(){
   url.base <- "http://fiis.com.br/"
   
   port %>%
-    select(ativo) %>%
+    select(ticker) %>%
     distinct() %>%
-    mutate( inform.url = paste0(url.base,ativo) ) %>%
+    mutate( inform.url = paste0(url.base,ticker) ) %>%
     mutate( html.page = inform.url %>% map(read_html) ) -> proventos.fetched
   
   proventos.fetched %>%
-    mutate( proventos = html.page %>% lapply(.processProventos) ) %>%
+    mutate( proventos = html.page %>% lapply(.processProventos) ) %>% 
     select( -inform.url, -html.page ) %>%
-    unnest() %>%
-    arrange( ativo, desc(data.pagamento) ) -> proventos.new
+    unnest() %>% View()
+    arrange( ticker, desc(data.pagamento) ) -> proventos.new
   
   if(file.exists(.PROVENTOS_FILENAME)){
     proventos <- readRDS(.PROVENTOS_FILENAME)
