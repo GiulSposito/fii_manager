93d39caa53e99f7b9dc020db44b26cb453a81617
Giuliano Sposito <gsposito@ciandt.com>
2019-03-12 08:53:05 -0300

    transformando script de executável em declarativo

--- R/import/pricesYahoo.R
+++ R/import/pricesYahoo.R
@@ -1,19 +1,39 @@
 library(BatchGetSymbols)
 library(lubridate)
+library(tidyverse)
 
-portfolio <- readRDS("./data/portfolio.rds")
+getPrices <- function(.tickers, .firstDate=NULL){
+  
+  if(is.null(.firstDate)) .firstDate=now()-years(2)
+  
+  .tickers %>% 
+    unique() %>% 
+    paste0(".SA") %>% 
+    BatchGetSymbols(
+      tickers = .,
+      first.date = .firstDate,
+      thresh.bad.data = 0.001
+    ) %>% 
+    return()
+}
 
-tickers <- portfolio$ticker %>% unique() %>% c("IFIX") %>% paste0(".SA")
-firstdate <- portfolio$date %>% min()
-
-cotacoes <- BatchGetSymbols(
+updatePortfolioPrices <- function(){
+  
+  portfolio <- readRDS("./data/portfolio.rds")
+  
+  tickers <- portfolio$ticker %>% unique() %>% c("IFIX") %>% paste0(".SA")
+  firstdate <- portfolio$date %>% min()
+  
+  cotacoes <- BatchGetSymbols(
     tickers = tickers,  
     first.date = firstdate,
     thresh.bad.data = 0.001
   )
+  
+  cotacoes$df.tickers %>%
+    as.tibble() %>%
+    mutate(ticker = gsub(".SA","",ticker)) %>% 
+    distinct() %>% 
+    saveRDS("./data/price.rds")
+}
 
-cotacoes$df.tickers %>%
-  as.tibble() %>%
-  mutate(ticker = gsub(".SA","",ticker)) %>% 
-  distinct() %>% 
-  saveRDS("./data/price.rds")
