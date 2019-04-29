cd2c9767e486fa47c83295e19eadcc84dad1858a
Giuliano Sposito <gsposito@ciandt.com>
2019-04-29 13:21:25 -0300

    Refatorando pipeline

--- R/pipeline/main.R
+++ R/pipeline/main.R
@@ -1,7 +1,11 @@
 # importacoes
-
 source("./R/import/portfolioGoogleSheets.R") # carteira de fundos
 source("./R/import/pricesYahoo.R")           # cotacoes da carteira
+
+# import (update) portifolio e tickers price
+portfolio <- updatePortfolio()
+price     <- updatePortfolioPrices(portfolio)
+
 
 # scrapping dos proventos das carteiras
 source("./R/import/proventos.R")             
