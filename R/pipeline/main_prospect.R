e4c346ff777343fd3a9e014958d10a48ada2d1b4
Giuliano Sposito <gsposito@ciandt.com>
2019-11-22 11:40:22 -0300

    changes...

--- R/pipeline/main_prospect.R
+++ R/pipeline/main_prospect.R
@@ -4,6 +4,13 @@ library(googlesheets)
 
 port  <- readRDS("./data/portfolio.rds")
 
+port %>% 
+  group_by(ticker) %>% 
+  summarise(
+    capital = sum(value, na.rm=T)
+  ) %>% 
+  arrange(desc(capital)) %>% View()
+
 
 # import data
 gs_auth()
@@ -17,7 +24,12 @@ sugestoes <- spreadsheet %>%
     in.portfolio = ticker %in% port$ticker
   )
 
-tickers <- c(sugestoes$ticker, port$ticker,"RBRF11", "MGFF11") %>% unique()
+# tickers <- c(sugestoes$ticker, port$ticker,"RBRF11", "MGFF11", 
+#              "VGIR11", "XPLG11", "BCRI11", "HGLG11", "RBRR11",
+#              "MGFF11") %>% unique()
+
+tickers <- port$ticker %>% c("HGCR11","CPTS11B", "PATC11")  %>% unique()
+
 
 # importacoes dos rendimentos
 source("./R/import/proventos.R")
@@ -29,52 +41,55 @@ source("./R/import/fixProventos.R")
 prov.fixed     <- fixProventos(proventos)
 prov           <- updateProventos(prov.fixed)
 
+saveRDS(prov, "./data/proventos.rds")
 
 # 'sharpe' de rendimentos
 # sugestoes %>%
 #   select(ticker, in.portfolio) %>%
 #   right_join(prov, by="ticker") %>%
-  
-prov %>% 
+
+port %>% 
+  group_by(ticker) %>% 
+  summarise(capital=sum(value)) %>% 
+  right_join(prov, by = "ticker") %>% 
   filter(!(ticker=="BRCR11" & data.base==ymd(20190313))) %>% #outlier do BRCR11 
   filter( data.pagamento >= now()-months(6) ) %>% 
-  group_by(ticker) %>%  #, in.portfolio) %>% 
+  group_by(ticker, capital) %>%  #, in.portfolio) %>% 
   summarise(
     retorno = mean(rendimento),
     volat   = sd(rendimento)
   ) %>% 
   ungroup() %>% 
   mutate(min.retorno = retorno - volat) %>% 
-  arrange(desc(min.retorno)) %>% View()
-  
-  
+  arrange(desc(min.retorno)) %>% 
+  select(-min.retorno) %>% 
   ggplot() +
-  geom_point(aes(x=volat, y=retorno, color=ticker)) + #, shape=in.portfolio)) + 
+  geom_point(aes(x=volat, y=retorno, color=ticker, size=capital)) + #, shape=in.portfolio)) + 
   theme_minimal() -> g
 
 ggplotly(g)
 
-XP:
-  RBRF11: 7000
-  RBED11: 3000 
-  VILG11: 4000
-  
-  
-
-Easy: IPO MGFF11 - 5200
+htmlwidgets::saveWidget(as_widget(ggplotly(g)), "fii_portfolio.html")
 
 
 # IPO MGFF11
 # Compra do RBRF11
 
-sel_tickers <- c("RBRF11", "RBED11", "VILG11", "VGIR11", "CBOP11", "HFOF11", "CEOC11", "HGRU11")
+sel_tickers <- c("HGCR11", "HGRU11", "GTWR11", "IRDM11")
 
 sugestoes %>% 
   filter(ticker %in% sel_tickers)
 
+port %>% 
+  group_by(ticker) %>% 
+  summarise(capital=sum(value, na.rm = T)) %>% 
+  arrange(capital) %>% 
+  head(10) %>% 
+  pull(ticker) -> sel_tickers
+
 # historico de rendimentos
 prov %>% 
-  filter(ticker %in% sel_tickers) %>% 
+  filter(ticker %in% sel_tickers) %>%  
   filter( data.pagamento >= now()-months(12) ) %>% 
   filter(!(ticker=="BRCR11" & data.base==ymd(20190313))) %>% #outlier do BRCR11 
   ggplot(aes(group=ticker)) +
