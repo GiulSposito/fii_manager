813f5c2d6323b7cd082a49cf2f2be2d2cd5b6970
Giuliano Sposito <gsposito@ciandt.com>
2020-03-16 10:00:52 -0300

    Pipe para compara proventos dos últimos 3 meses contra a última cotação a fim de achar os FIIs do portifolio mais valorizados do pronto de vista de rendimentos mensais

---
+++ R/_draft/renew_pipe.R
@@ -0,0 +1,58 @@
+source("./R/api/ffis_api.R")
+source("./R/import/portfolioGoogleSheets.R")
+
+port <- updatePortfolio()
+
+my_tickers <- port %>% 
+  select(ticker) %>% 
+  distinct()
+
+safe_getFIIInfo <- safely(.f = getFIIinfo, otherwise = list(), quiet = F)
+
+fiis_site <- my_tickers %>% 
+  mutate(info = map(ticker, safe_getFIIInfo))
+
+fii_info <- fiis_site %>% 
+  mutate(return_ok=map_lgl(info, function(.x) is.null(.x$error)))
+  filter(return_ok) %>% 
+  mutate(
+    price     = map(info, ~.x$result$price),    
+    proventos = map(info, ~.x$result$proventos),    
+    updates   = map(info, ~.x$result$updagtes)    
+  ) %>%
+  select(ticker, price, proventos, updates)
+
+last_prices <- fii_info %>% 
+  select(ticker, price) %>% 
+  unnest(price) %>% 
+  select(-ticker1) %>% 
+  group_by(ticker) %>% 
+  filter( date.ref == max(date.ref)) %>% 
+  ungroup()
+
+last_prov <- fii_info %>% 
+  select(ticker, proventos) %>% 
+  unnest(proventos) %>% 
+  group_by(ticker) %>% 
+  top_n(3,data.base) %>% 
+  summarise(
+    valor = mean(valor), 
+    cota.base = mean(cota.base)
+  ) %>% 
+  ungroup() %>% 
+  mutate(rendimento=100*valor/cota.base)
+
+fii_indicados <- last_prov %>% 
+  inner_join(last_prices, by = "ticker") %>% 
+  select(ticker, valor, cota.base, rendimento, date.ref, price.close) %>% 
+  mutate(novo.rend = 100*valor/price.close) %>% 
+  arrange(desc(novo.rend)) 
+
+capital <- port %>% 
+  group_by(ticker) %>% 
+  summarise(capital=sum(value))
+
+
+fii_indicados %>% 
+  inner_join(capital, by="ticker") %>% 
+  arrange(desc(novo.rend))
